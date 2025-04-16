c1ceb21027c82c36089e600f9857295afa099a94
Mamoudou NABE <mn420660@MI115-02.iem>
2025-04-16 13:49:54 +0200

    maj

--- test/src/app/accueil/accueil.component.ts
+++ test/src/app/accueil/accueil.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-accueil',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MatListModule, MatIconModule],
   templateUrl: './accueil.component.html',
   styleUrl: './accueil.component.scss'
 })
